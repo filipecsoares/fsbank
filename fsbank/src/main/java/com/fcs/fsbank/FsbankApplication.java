712285872eadcd2aca09e6a01bbfe6d41b7c7ce9
filipecsoares <filipecarlossoares@gmail.com>
2024-08-14 16:05:01 -0300

    init project

---
+++ fsbank/src/main/java/com/fcs/fsbank/FsbankApplication.java
@@ -0,0 +1,13 @@
+package com.fcs.fsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsbankApplication.class, args);
+	}
+
+}
---
+++ fsbank/src/test/java/com/fcs/fsbank/FsbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcs.fsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
