96db297d93f1267249d89fd2b7c139dbdb42390d
filipecsoares <filipecarlossoares@gmail.com>
2024-08-15 15:31:17 -0300

    dont let user create a compromised password

--- fsbank/src/main/java/com/fcs/fsbank/config/SecurityConfig.java
+++ fsbank/src/main/java/com/fcs/fsbank/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fcs.fsbank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -28,13 +30,18 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{bcrypt}$2a$12$4tOzsvQuOaiEBcoU0r90N.PkjQl7CanX7wILGUs48RcHRTKpja2O6").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$1FCBMnoSYdUix/bBAEOqAu3Oa4NL8PoTRo7u/f.30nugEAGaOeize").authorities("admin").build();
+        UserDetails user = User.withUsername("user").password("{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu").authorities("admin").build();
         return new InMemoryUserDetailsManager(user, admin);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public CompromisedPasswordChecker compromisedPasswordChecker() {
+        return new HaveIBeenPwnedRestApiPasswordChecker();
     }
 }
