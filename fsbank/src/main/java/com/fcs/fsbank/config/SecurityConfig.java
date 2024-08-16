f377f1bae7f684cd1ef4b71fa4ac16519adc6824
filipecsoares <filipecarlossoares@gmail.com>
2024-08-16 14:06:08 -0300

    fix: h2 blocked

--- fsbank/src/main/java/com/fcs/fsbank/config/SecurityConfig.java
+++ fsbank/src/main/java/com/fcs/fsbank/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,16 +14,24 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(req -> req
                 .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
-                .requestMatchers("/contact", "/notices", "/error").permitAll());
+                .requestMatchers("/contact", "/notices", "/error").permitAll()
+                .requestMatchers(toH2Console()).permitAll());
+        http.csrf(csrf -> csrf.ignoringRequestMatchers(toH2Console()));
+        http.headers(headers -> headers
+                .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable
+                )
+        );
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
