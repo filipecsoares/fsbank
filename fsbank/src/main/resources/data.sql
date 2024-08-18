bf56f896ffd90d89c3b744b30773816a53b98d21
filipecsoares <filipecarlossoares@gmail.com>
2024-08-18 14:47:46 -0300

    create customer table

--- fsbank/src/main/resources/data.sql
+++ fsbank/src/main/resources/data.sql
@@ -2,7 +2,19 @@ create table users(username varchar(50) not null primary key,password varchar(50
 create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username,authority);
 
-insert into users(username,password,enabled) values ('user','{noop}password',true);
-insert into users(username,password,enabled) values ('admin','{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu',true);
-insert into authorities(username,authority) values ('user','ROLE_USER');
-insert into authorities(username,authority) values ('admin','ROLE_ADMIN');
+INSERT IGNORE INTO `users` VALUES ('user', '{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu', '1');
+INSERT IGNORE INTO `authorities` VALUES ('user', 'read');
+
+INSERT IGNORE INTO `users` VALUES ('admin', '{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu', '1');
+INSERT IGNORE INTO `authorities` VALUES ('admin', 'admin');
+
+CREATE TABLE `customer` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(45) NOT NULL,
+    `pwd` varchar(200) NOT NULL,
+    `role` varchar(45) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('happy@example.com', '{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu', 'read');
+INSERT  INTO `customer` (`email`, `pwd`, `role`) VALUES ('admin@example.com', '{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu', 'admin');
