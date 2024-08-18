63832fa10fd5fc97845b278fe08e31b7e2e9ae39
filipecsoares <filipecarlossoares@gmail.com>
2024-08-18 14:32:23 -0300

    get users from database

--- fsbank/src/main/java/com/fcs/fsbank/config/SecurityConfig.java
+++ fsbank/src/main/java/com/fcs/fsbank/config/SecurityConfig.java
@@ -5,14 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -38,10 +38,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu").authorities("read").build();
-        UserDetails admin = User.withUsername("admin").password("{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu").authorities("admin").build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
--- fsbank/src/main/resources/data.sql
+++ fsbank/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 create table users(username varchar(50) not null primary key,password varchar(500) not null,enabled boolean not null);
 create table authorities (username varchar(50) not null,authority varchar(50) not null,constraint fk_authorities_users foreign key(username) references users(username));
 create unique index ix_auth_username on authorities (username,authority);
+
+insert into users(username,password,enabled) values ('user','{noop}password',true);
+insert into users(username,password,enabled) values ('admin','{bcrypt}$2a$12$B9Lo5MQs6VA0aHKifZXmRe1sho71o/7ul6ahVoQUCe2gM9M.P8JCu',true);
+insert into authorities(username,authority) values ('user','ROLE_USER');
+insert into authorities(username,authority) values ('admin','ROLE_ADMIN');
