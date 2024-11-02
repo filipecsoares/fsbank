e0fd3c69ae297885a13beabe7ec3a5c0adf413c0
filipecsoares <filipecarlossoares@gmail.com>
2024-11-02 18:04:51 -0300

    config: enable debug web security

--- fsbank/src/main/java/com/fcs/fsbank/FsbankApplication.java
+++ fsbank/src/main/java/com/fcs/fsbank/FsbankApplication.java
@@ -2,8 +2,10 @@ package com.fcs.fsbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity(debug = true)
 public class FsbankApplication {
 
 	public static void main(String[] args) {
